fd29a511c31c6e620dfba0c6b2f71427619ba34a
digitalnomad91 <andrew.c.corbin@gmail.com>
2026-01-25 17:33:43 -0600

    Path resolution updates.

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,17 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@": "./"
+          },
+          extensions: [".ios.js", ".android.js", ".js", ".ts", ".tsx", ".json"]
+        }
+      ]
+    ],
   };
 };
